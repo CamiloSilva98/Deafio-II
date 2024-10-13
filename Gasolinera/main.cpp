7626e06667f1a6ec926af8d546684b3794623e99
CamiloSilva98 <36543134+CamiloSilva98@users.noreply.github.com>
2024-10-13 18:21:58 -0500

    Update main.cpp

--- Gasolinera/main.cpp
+++ Gasolinera/main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
-#include <string>
-#include "rednacional.h"
-#include "estacionservicio.h"
-#include "surtidor.h"
-#include "tanque.h"
+#include <cstdlib>
+#include <ctime>
+#include "RedNacional.h"
+using namespace std;
+
+void mostrarMenu() {
+    cout << "\n--- Menu Principal ---\n";
+    cout << "1. Gestion de la red nacional\n";
+    cout << "2. Gestion de estaciones de servicio\n";
+    cout << "3. Verificacion de fugas\n";
+    cout << "4. Simulacion de ventas\n";
+    cout << "0. Salir\n";
+    cout << "Seleccione una opcion: ";
+}
+
+int main() {
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    RedNacional redNacional;
+    int opcion;
+
+    do {
+        mostrarMenu();
+        cin >> opcion;
+
+        switch (opcion) {
+        case 1:
+            redNacional.gestionRedNacional();
+            break;
+        case 2:
+            redNacional.gestionEstacionesServicio();
+            break;
+        case 3:
+            redNacional.verificacionFugas();
+            break;
+        case 4:
+            redNacional.simulacionVentas();
+            break;
+        case 0:
+            cout << "Saliendo del programa...\n";
+            break;
+        default:
+            cout << "Opción no valida. Intente de nuevo.\n";
+        }
+    } while (opcion != 0);
+
+    return 0;
+}
