035624e912072350a3b7a7a5a90e8941bf4d2627
ChenteZabaleta <vicente.zabaleta@udea.edu.co>
2024-10-13 20:23:34 -0500

    Update tanque.h

--- Gasolinera/tanque.h
+++ Gasolinera/tanque.h
@@ -2,22 +2,28 @@
 #define TANQUE_H
 
 #include <string>
+using namespace std;
 
 class Tanque {
 private:
-    float capacidadRegular;
-    float capacidadPremium;
-    float capacidadEcoExtra;
-    float precioRegular;
-    float precioPremium;
-    float precioEcoExtra;
+    double capacidadRegular;
+    double capacidadPremium;
+    double capacidadEcoExtra;
+    double cantidadRegular;
+    double cantidadPremium;
+    double cantidadEcoExtra;
+    double precioRegular;
+    double precioPremium;
+    double precioEcoExtra;
 
 public:
-    Tanque(float capReg = 150, float capPre = 150, float capEco = 150);
-
-    void setPrecio(float reg, float pre, float eco);
-    float getPrecio(const std::string& categoria) const;
-    bool venderCombustible(float cantidad, const std::string& categoria);
+    Tanque();
+    void asignarCapacidades();
+    void restarCantidad(const string& categoria, double cantidad);
+    double obtenerCantidad(const string& categoria) const;
+    double obtenerPrecio(const string& categoria) const;
+    void fijarPrecios(double regular, double premium, double ecoExtra);
+    void verificarFugas() const;
 };
 
 #endif // TANQUE_H
